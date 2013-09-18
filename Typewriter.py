3c816c5e76ec4e679e1f362620e8cc8f6b1099a6
Alex Armstrong <alehandrof@gmail.com>
2013-09-18 22:03:22 +0300

    New & improved offset implementation for scrolling mode
    Inelegant code, but robust solution to #3 (AFAICT).

--- Typewriter.py
+++ Typewriter.py
@@ -1,7 +1,7 @@
-# http://www.sublimetext.com/forum/viewtopic.php?f=6&t=4806
-# created by:
+# originally created by:
 # + castles_made_of_sand
 # + facelessuser
+# http://www.sublimetext.com/forum/viewtopic.php?f=6&t=4806
 
 import sublime, sublime_plugin
 
@@ -11,9 +11,14 @@ class TypewriterScrolling(sublime_plugin.EventListener):
             sel = view.sel()
             region = sel[0] if len(sel) == 1 else None
             if region != None:
-                view.show_at_center(region)
                 offset = sublime.load_settings('Typewriter.sublime-settings').get('typewriter_mode_scrolling_offset', 0.0)
                 if offset != 0:
-                    cur_v = view.viewport_position()
-                    new_v = (cur_v[0], cur_v[1] + (view.line_height() * offset) )
-                    view.set_viewport_position(new_v)
+                    offset = offset * view.line_height()
+                    if offset > view.viewport_extent()[1] / 2:
+                        print("Typewriter: The cursor is not within the viewport! Use a smaller offset value.")
+                    region_a    = list(view.text_to_layout(region.a))
+                    region_b    = list(view.text_to_layout(region.b))
+                    region_a[1] = region_a[1] + offset
+                    region_b[1] = region_b[1] + offset
+                    region      = sublime.Region(view.layout_to_text(region_a), view.layout_to_text(region_b))
+                view.show_at_center(region)
