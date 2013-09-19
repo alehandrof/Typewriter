f6548ecc4469e2087256d57d67b03439e2efea74
Alex Armstrong <alehandrof@gmail.com>
2013-09-19 21:37:50 +0300

    Refactor offset code
    Now works in ST2, as well.

--- Typewriter.py
+++ Typewriter.py
@@ -5,20 +5,22 @@
 
 import sublime, sublime_plugin
 
+def offset_point(view, point):
+    vector = list( view.text_to_layout(point) )
+    vector[1] = vector[1] + offset
+    return view.layout_to_text( tuple(vector) )
+
 class TypewriterScrolling(sublime_plugin.EventListener):
     def on_selection_modified(self, view):
         if view.settings().get('typewriter_mode_scrolling') == 1:
             sel = view.sel()
             region = sel[0] if len(sel) == 1 else None
             if region != None:
-                offset = sublime.load_settings('Typewriter.sublime-settings').get('typewriter_mode_scrolling_offset', 0.0)
+                global offset
+                offset = sublime.load_settings('Typewriter.sublime-settings').get('typewriter_mode_scrolling_offset', 0.0) * view.line_height()
                 if offset != 0:
-                    offset = offset * view.line_height()
-                    if offset > view.viewport_extent()[1] / 2:
-                        print("Typewriter: The cursor is not within the viewport! Use a smaller offset value.")
-                    region_a    = list(view.text_to_layout(region.a))
-                    region_b    = list(view.text_to_layout(region.b))
-                    region_a[1] = region_a[1] + offset
-                    region_b[1] = region_b[1] + offset
-                    region      = sublime.Region(view.layout_to_text(region_a), view.layout_to_text(region_b))
+                    if offset > (view.viewport_extent()[1] / 2):
+                        print("Typewriter Error: The cursor is outside the viewport! Use a smaller offset value.")
+                    else:
+                        region = sublime.Region(offset_point(view, region.a), offset_point(view, region.b))
                 view.show_at_center(region)
