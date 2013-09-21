941c903c1c4dfbfde1aa41360bf8533c6d021fdc
Alex Armstrong <alehandrof@gmail.com>
2013-09-21 10:25:01 +0300

    Clean up script a little bit

--- Typewriter.py
+++ Typewriter.py
@@ -5,13 +5,18 @@
 
 import sublime, sublime_plugin
 
-def offset_point(view, point):
-    vector = list( view.text_to_layout(point) )
-    vector[1] = vector[1] + offset
-    return view.layout_to_text( tuple(vector) )
 
 class TypewriterScrolling(sublime_plugin.EventListener):
+    # def run (self, view):
+        # self.center_view(view)
+
+    # def on_deactivated(self, view):
+        # self.center_view(view)
+
     def on_selection_modified(self, view):
+        self.center_view(view)
+
+    def center_view(self, view):
         if view.settings().get('typewriter_mode_scrolling') == 1:
             sel = view.sel()
             region = sel[0] if len(sel) == 1 else None
@@ -22,5 +27,10 @@ class TypewriterScrolling(sublime_plugin.EventListener):
                     if offset > (view.viewport_extent()[1] / 2):
                         print("Typewriter Error: The cursor is outside the viewport! Use a smaller offset value.")
                     else:
-                        region = sublime.Region(offset_point(view, region.a), offset_point(view, region.b))
+                        region = sublime.Region(self.offset_point(view, region.a), self.offset_point(view, region.b))
                 view.show_at_center(region)
+
+    def offset_point(self, view, point):
+        vector = list( view.text_to_layout(point) )
+        vector[1] = vector[1] + offset
+        return view.layout_to_text( tuple(vector) )
